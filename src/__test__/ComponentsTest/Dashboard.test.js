c879c6e8edaff9aec54f7cd54e1fbef4f8084e32
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-02-08 15:53:44 +0500

    Update Dashboard.test.js
    Remove eslint errors.

--- src/__test__/ComponentsTest/Dashboard.test.js
+++ src/__test__/ComponentsTest/Dashboard.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,16 +6,16 @@ import '@testing-library/jest-dom';
 import Dashboard from '../../pages/Dashboard';
 import store from '../../Redux/configureStore.js';
 
-
 describe('Dashboard', () => {
-    test('renders correctly', () => {
-      const tree = renderer.create(
-        <Provider store={store}>
+  test('renders correctly', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
         <Router>
           <Dashboard />
-        </Router>,
-        </Provider>
-      ).toJSON();
-      expect(tree).toMatchSnapshot();
-    });
+        </Router>
+        ,
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
   });
+});
